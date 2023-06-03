0f9b9355b26ad748294909611fd1d5b417d3c8d7
Sandra S <zandrastr@hotmail.com>
2023-06-03 13:20:47 +0200

    remove player input component from app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Gameboard from './components/Gameboard.vue';
-import PlayerInput from './components/PlayerInput.vue';
 </script>
 
 <template>
   <h1>Tic Tac Toe</h1>
-  <PlayerInput></PlayerInput>
   <Gameboard></Gameboard>
 </template>
 
