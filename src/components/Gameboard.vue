fd64203bef58df40f0301f8e258369f5020aaaf8
Sandra S <zandrastr@hotmail.com>
2023-06-04 19:14:46 +0200

    add functions for player names and message

--- src/components/Gameboard.vue
+++ src/components/Gameboard.vue
@@ -2,40 +2,61 @@
 import { ref, Ref } from 'vue'; //imports ref function and Ref type from vue
 import PlayerInput from './PlayerInput.vue';
 
+let userOneInput = ref('');
+let userTwoInput = ref('');
 let currentPlayer = ref(1);
-
 let gameboardSquares= ref<string[]>( ['','','','','','','','','']);
+let playerMessage = ref('');
+let winnerMessage = ref('');
+let isDraw: boolean = false;
+let isGameOver: boolean = false;
 
-console.log(gameboardSquares.value);
+function setPlayerNames(players: any) {
+    userOneInput.value = players.userOne;
+    userTwoInput.value = players.userTwo;
+    console.log('User One Input:', userOneInput.value);
+    console.log('User Two Input:', userTwoInput.value);
+    addPlayerMessage();
+}
+
+function addPlayerMessage(){
+    currentPlayer.value === 1 ? playerMessage.value = "Its your turn " + userOneInput.value + "!" : playerMessage.value = "Its your turn " + userTwoInput.value + "!";
+}
 
 function placeSymbol(index: number){
-    //add value of X or O to square
-    //update current player
-
-    console.log("placeSymbol", index);
-    console.log(gameboardSquares.value[index]);
-    console.log('currentPlayer --->', currentPlayer.value);
-
     if(currentPlayer.value === 1){
         gameboardSquares.value[index] = 'X';
         currentPlayer.value = 2;
+        addPlayerMessage();
+        winnerCalculator();
     }
     else{
         gameboardSquares.value[index] = 'O';
         currentPlayer.value = 1;
+        addPlayerMessage();
+        winnerCalculator();
     }
+}
+
+function winnerCalculator(){
+    console.log("Winner calculator");
 }
 </script>
 
 <!-- ********************************************************************************** -->
 
 <template>
-    <PlayerInput></PlayerInput>
+    <PlayerInput @setPlayerNames="setPlayerNames"></PlayerInput>
     <div class="gameboardWrapper">
         <div class="gameboard">
-            <div class="gameboardSquare" v-for="(square, index) in gameboardSquares" :key="index" @click.once="placeSymbol(index)"><p>{{ gameboardSquares[index] }}</p></div> <!-- List rendering, loops through the array, empty string  -->
+            <div class="gameboardSquare" v-if="!isGameOver" v-for="(square, index) in gameboardSquares" :key="index" @click.once="placeSymbol(index)"><p>{{ gameboardSquares[index] }}</p></div> <!-- List rendering, loops through the array, empty string  -->
+            <p v-if="isGameOver">Play again</p>
         </div>
     </div>
+    <p v-if="!isGameOver && !isDraw" > {{ playerMessage }} </p>
+    <p v-if="isGameOver && !isDraw" > {{ winnerMessage }} </p>
+    <p v-if="isDraw === true"> It's a Tie! </p>
+
 </template>
 
 <!-- ********************************************************************************** -->
